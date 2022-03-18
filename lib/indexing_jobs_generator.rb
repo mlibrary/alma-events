c660d3720eef11ceeda2c8851d7db460f157b918
Monique Rio <mrio@umich.edu>
2022-03-18 15:13:32 -0400

    changed sftp wrapper class to use IdentityFile

--- lib/indexing_jobs_generator.rb
+++ lib/indexing_jobs_generator.rb
@@ -39,10 +39,11 @@ class SFTP
   def initialize
     @user = ENV.fetch('ALMA_FILES_USER')
     @host = ENV.fetch('ALMA_FILES_HOST')
+    @key = ENV.fetch('SSH_KEY_PATH')
   end
   #returns an array of items in a directory
   def ls(path="")
-    array = ["sftp", "-b", "-",  "#{@user}@#{@host}", 
+    array = ["sftp", "-oIdentityFile=#{@key}", "-b", "-",  "#{@user}@#{@host}", 
       "<<<", "$'@ls #{path}'"]
     command = array.join(" ")
     `bash -c \"#{command}\"`.split("\n").map{|x| x.strip}
